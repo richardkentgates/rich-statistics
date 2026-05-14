d9a6c733631e453b314957a7b9d3e88beb2cd07b
Richard Kent Gates <mail@richardkentgates.com>
2026-05-14 15:55:29 -0500

    fix: cast schema version option to int in test assertion

--- tests/integration/DbTest.php
+++ tests/integration/DbTest.php
@@ -145,7 +145,7 @@ class DbTest extends WP_UnitTestCase {
 	}
 
 	public function test_schema_version_option_matches_constant(): void {
-		$this->assertSame( (string) RSA_DB::SCHEMA_VERSION, get_option( RSA_DB::OPTION_KEY ) );
+		$this->assertSame( RSA_DB::SCHEMA_VERSION, (int) get_option( RSA_DB::OPTION_KEY ) );
 	}
 
 	public function test_events_table_has_all_expected_columns(): void {
